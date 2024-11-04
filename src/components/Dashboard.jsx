e5897c47b610f710a1abefe2f0f6f56aba1e2d9a
BensonEkwo <bensoneekwo077@gmail.com>
2024-11-04 17:58:02 +0100

    few touches

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -25,7 +25,7 @@ function Dashboard() {
         <span className='text-2xl'><MdOutlineKeyboardBackspace /></span> 
         </Link>
         <span>
-          <h1 className='text-nowrap text-lg font-space font-semibold'>AlgoHub Wallet</h1>
+          <h1 className='text-nowrap text-lg font-space font-semibold'>AlgoHub Dashboard</h1>
           </span></div>
           <div className='inline-flex space-x-4'>
           <span className='text-xl'> <IoIosArrowDown /></span>
@@ -35,7 +35,7 @@ function Dashboard() {
       <div className='flex ittems-center justify-between px-4 border-b-2 border-gray-100
       pb-2'>
        <img src='/avatar.jpeg' alt='avatar' className='w-[30px] h-[30px] rounded-full object-cover'/>
-        <span className='font-space text-xl' style={{color:'#31D8EE'}}>AlgoHub</span><span className='text-2xl'><LuBell /></span>
+        <span className='font-space text-xl font-semibold' style={{color:'#31D8EE'}}>AlgoHub</span><span className='text-2xl'><LuBell /></span>
       </div>
       <div className='flex items-center justify-center mt-2'>
       <div className='flex items-center p-2 my-6 space-x-1 rounded-md' style={{backgroundColor:'#CCF9FF'}}>
@@ -49,19 +49,19 @@ function Dashboard() {
           style={{backgroundColor:'#31D8EE'}}>
           </div>
          <div className='flex items-center justify-between mb-5 '>
-         <div><h1 className='text-white font-space'> <span className='inline-flex gap-2 items-center'>My Wallet  < IoMdWallet/> </span>
+         <div className='z-50'><h1 className='text-white font-space'> <span className='inline-flex gap-2 items-center font-medium '>My Wallet  < IoMdWallet/> </span>
         </h1> 
-         <span className='inline-flex gap-2 items-center text-white'>0x2xvv.....d34dhdcv<MdContentCopy /></span>
+         <span className='inline-flex gap-2 items-center text-white font-medium'>0x2xvv.....d34dhdcv<MdContentCopy /></span>
          </div>
-          <div className='p-1 bg-white rounded-xl inline-flex items-center'><span className='text-xs'>USD</span> <span className='text-xs'><IoIosArrowDown /></span></div>
+          <div className='p-1 bg-white rounded-xl inline-flex items-center z-50'><span className='text-xs'>USD</span> <span className='text-xs'><IoIosArrowDown /></span></div>
          </div>
          <div className='flex gap-2 '>
-            <div className='bg-white w-[46px] h-[46px] rounded-full flex items-center justify-center'>
+            <div className='bg-white w-[46px] h-[46px] rounded-full flex items-center justify-center z-50'>
               <img src='/algologo.png' alt='Algo logo' className='w-[31px] h-[31px]'/>
             </div> 
-            <div className='flex flex-col'>
-              <span className='font-space text-white'> {Algo_Balance} 'ALGO'</span>
-              <span className='font-noto text-white'>
+            <div className='flex flex-col z-50'>
+              <span className='font-space text-white font-medium text-xl'> {Algo_Balance} ALGO</span>
+              <span className='font-noto text-white font-medium'>
                 {Usd_Balance}
               </span>
             </div>
@@ -71,13 +71,13 @@ function Dashboard() {
       <div className='w-full my-8'>
         <div className='flex items-center justify-evenly'>
      <Link to='/sendtoken'>
-     <button className='flex flex-col items-center gap-2'> <span className='rounded-full p-3'
-      style={{backgroundColor:'#CCF9FF'}}><FiSend/></span> <span className='block text-sm font-mono'>Send</span></button>
+     <button className='flex flex-col items-center gap-2'> <span className='rounded-full p-4'
+      style={{backgroundColor:'#CCF9FF'}}><FiSend/></span> <span className='block text-base font-mono'>Send</span></button>
      </Link>
-      <button className='flex flex-col items-center gap-2'><span className='rounded-full p-3' 
-      style={{backgroundColor:'#CCF9FF'}}><BiCategory/></span> <span className='block text-sm font-mono'>Receive</span></button>
-     <button className='flex flex-col items-center gap-2'> <span className='rounded-full p-3'
-     style={{backgroundColor:'#CCF9FF'}}><IoMdSwap/></span> <span className='block text-sm font-mono'>Swap</span></button>
+      <button className='flex flex-col items-center gap-2'><span className='rounded-full p-4' 
+      style={{backgroundColor:'#CCF9FF'}}><BiCategory/></span> <span className='block text-base font-mono'>Receive</span></button>
+     <button className='flex flex-col items-center gap-2'> <span className='rounded-full p-4'
+     style={{backgroundColor:'#CCF9FF'}}><IoMdSwap/></span> <span className='block text-base font-mono'>Swap</span></button>
         </div>
       </div>
       <div className='flex flex-col w-full px-3 mt-3'>
@@ -93,11 +93,11 @@ function Dashboard() {
             <img src='/usdt.jpeg' alt='usdt' className='w-[34px] h-[34px]'/>
           </div>
           <div className='flex flex-col items-center'>
-            <span className='block text-xs font-space font-medium'>USDT</span> <span className='block text-xs '>'$1.99'</span>
+            <span className='block text-xs font-space font-medium'>USDT</span> <span className='block text-xs '>$1.99</span>
           </div>
           </div>
           <div className='flex flex-col items-end'>
-            <span className='block text-xs'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>'+0.01%'</span>
+            <span className='block text-xs'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>+0.01%</span>
           </div>
         </div>
         <div className='flex items-center justify-between my-3'>
@@ -106,11 +106,11 @@ function Dashboard() {
             <img src='/usdc.jpeg' alt='usdc' className='w-[34px] h-[34px]'/>
           </div>
           <div className='flex flex-col items-center'>
-            <span className='block text-sm font-space font-medium'>USDC</span> <span className='block text-xs '>'$1.99'</span>
+            <span className='block text-sm font-space font-medium'>USDC</span> <span className='block text-xs '>$1.99</span>
           </div>
           </div>
           <div className='flex flex-col items-end'>
-            <span className='block text-xs'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>'+0.01%'</span>
+            <span className='block text-xs'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>+0.01%</span>
           </div>
         </div>
         <div className='flex items-center justify-between my-3'>
@@ -119,11 +119,11 @@ function Dashboard() {
             <img src='/avax.jpeg' alt='avax' className='w-[34px] h-[34px]'/>
           </div>
           <div className='flex flex-col items-center'>
-            <span className='block text-sm font-space font-medium'>AVAX</span> <span className='block text-xs '>'$1.99'</span>
+            <span className='block text-sm font-space font-medium'>AVAX</span> <span className='block text-xs '>$1.99</span>
           </div>
           </div>
           <div className='flex flex-col items-end'>
-            <span className='block text-sm'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>'+0.01%'</span>
+            <span className='block text-sm'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>+0.01%</span>
           </div>
         </div>
         <div className='flex items-center justify-between my-3'>
@@ -132,11 +132,11 @@ function Dashboard() {
             <img src='/pdt.jpeg' alt='Polk' className='w-[34px] h-[34px]'/>
           </div>
           <div className='flex flex-col items-center'>
-            <span className='block text-sm font-space font-medium'>Polk</span> <span className='block text-xs '>'$1.99'</span>
+            <span className='block text-sm font-space font-medium'>Polk</span> <span className='block text-xs '>$1.99</span>
           </div>
           </div>
           <div className='flex flex-col items-end'>
-            <span className='block text-xs'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>'+0.01%'</span>
+            <span className='block text-xs'>0</span> <span className='block text-xs' style={{color:'#01AB73'}}>+0.01%</span>
           </div>
         </div>
       </div>
