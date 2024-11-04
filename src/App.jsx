d35c202444cae941d1950974f4047fe6fccc0f75
BensonEkwo <bensoneekwo077@gmail.com>
2024-11-04 16:35:48 +0100

    telegram theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 
 import { Outlet } from "react-router-dom"
-
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    
+    Telegram.WebApp.ready();
+    
+    document.body.style.backgroundColor = "#ffffff";
 
+    Telegram.WebApp.onEvent('themeChanged', () => {
+      document.body.style.backgroundColor = "#ffffff";
+    });
 
+    return () => {
+      Telegram.WebApp.offEvent('themeChanged');
+    };
+  }, []);
   return (
     <>
     <Outlet/>
